3150b17fb5965902d631e203c11495eea398a336
trteel1 <tateelamino@gmail.com>
2023-01-27 11:55:42 +0300

    Completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const actorsItem = actors.map((actor) => (
+    <div key={actor.name}>
+      {actor.name}
+      <ul>
+        {actor.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+  return <div>
+    <h1>Actors Page</h1>
+    {actorsItem}
+    </div>;
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorsItem = directors.map((director) => (
+    <div key={director.name}>
+      {director.name}
+      <ul>
+        {director.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+  return <div>
+    <h1>Directors Page</h1>
+    {directorsItem}
+    </div>;
 }
 
 export default Directors;
